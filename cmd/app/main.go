de8ecb85e853df758657f14f9212073f0741b5b8
BlinkingLight <into@ituoga.lt>
2024-10-16 10:03:01 +0300

    js, css, layout

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,28 +1,77 @@
 package main
 
 import (
-	"time"
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/gorilla/sessions"
-	"github.com/invopop/ctxi18n"
-	"github.com/ituoga/go-start/locales"
+	"github.com/ituoga/go-start/handlers"
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	if err := ctxi18n.LoadWithDefault(locales.Content, "en"); err != nil {
-		panic(err)
+var port = ":8080"
+
+func init() {
+	if p := os.Getenv("BIND"); p != "" {
+		port = p
 	}
 
-	router := chi.NewRouter()
+}
 
-	router.Use(middleware.RequestID)
-	router.Use(middleware.RealIP)
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info(fmt.Sprintf("Starting Server @%s", port))
+	defer logger.Info("Stopping Server")
 
-	sessionStore := sessions.NewCookieStore([]byte("secret")) // @todo: move to env
-	sessionStore.MaxAge(int(24 * time.Hour / time.Second))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
+	if err := run(ctx, logger); err != nil {
+		logger.Error("Error running server", slog.Any("err", err))
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, logger *slog.Logger) error {
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(startServer(ctx, logger, port))
+
+	if err := g.Wait(); err != nil {
+		return fmt.Errorf("error running server: %w", err)
+	}
+
+	return nil
+}
+
+func startServer(ctx context.Context, logger *slog.Logger, port string) func() error {
+	return func() error {
+		router := chi.NewMux()
+
+		router.Use(middleware.RequestID)
+		router.Use(middleware.RealIP)
+		router.Use(middleware.Logger)
+		router.Use(middleware.Recoverer)
+
+		router.Handle("/*", static())
+
+		handlers.SetupRoutes(logger, router)
+
+		srv := &http.Server{
+			Addr:    fmt.Sprintf("%s", port),
+			Handler: router,
+		}
+
+		go func() {
+			<-ctx.Done()
+			srv.Shutdown(context.Background())
+		}()
+
+		return srv.ListenAndServe()
+	}
 }
--- handlers/setup_home.go
+++ handlers/setup_home.go
@@ -3,15 +3,19 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/delaneyj/toolbelt/embeddednats"
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/sessions"
+	"github.com/ituoga/go-start/web/views/demoview"
 )
 
-func SetupHome(router chi.Router, session sessions.Store) {
+func SetupHome(router chi.Router, session sessions.Store, ns *embeddednats.Server) error {
 
 	homeHandler := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Home"))
+		demoview.Demo().Render(r.Context(), w)
 	}
 
 	router.Get("/", homeHandler)
+
+	return nil
 }
