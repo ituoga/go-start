55443ab61c3948feda91415a03c29f45e06fcedd
BlinkingLight <into@ituoga.lt>
2024-10-19 13:41:24 +0300

    fixed migrations path and added home template

--- handlers/setup_home.go
+++ handlers/setup_home.go
@@ -6,12 +6,13 @@ import (
 	"github.com/delaneyj/toolbelt/embeddednats"
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/sessions"
+	"github.com/ituoga/go-start/web/views/homeview"
 )
 
 func SetupHome(router chi.Router, session sessions.Store, ns *embeddednats.Server) error {
 
 	homeHandler := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Home"))
+		homeview.Index().Render(r.Context(), w)
 	}
 
 	router.Get("/", homeHandler)
--- web/views/layoutview/layout_templ.go
+++ web/views/layoutview/layout_templ.go
@@ -8,6 +8,8 @@ package layoutview
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/delaneyj/datastar"
+
 func Main() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -29,7 +31,15 @@ func Main() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html data-theme=\"light\"><head><title></title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link href=\"/static/index.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><div class=\"navbar bg-base-100\"><div class=\"navbar-start\"><div class=\"dropdown\"><div tabindex=\"0\" role=\"button\" class=\"btn btn-ghost lg:hidden\"><svg xmlns=\"http://www.w3.org/2000/svg\" class=\"h-5 w-5\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M4 6h16M4 12h8m-8 6h16\"></path></svg></div><ul tabindex=\"0\" class=\"menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow\"><li><a>Item 1</a></li><li><a>Parent</a><ul class=\"p-2\"><li><a>Submenu 1</a></li><li><a>Submenu 2</a></li></ul></li><li><a>Item 3</a></li></ul></div><a class=\"btn btn-ghost text-xl\">daisyUI</a></div><div class=\"navbar-center hidden lg:flex\"><ul class=\"menu menu-horizontal px-1\"><li><a>Item 1</a></li><li class=\"z-10\"><details><summary>Parent</summary><ul class=\"p-2 min-w-48\"><li><a>Submenu 1</a></li><li><a>Submenu 2</a></li></ul></details></li><li><a>Item 3</a></li></ul></div><div class=\"navbar-end\"><div class=\"dropdown dropdown-end\"><div tabindex=\"0\" role=\"button\" class=\"btn btn-ghost btn-circle avatar\"><div class=\"w-10 rounded-full\"><img alt=\"Tailwind CSS Navbar component\" src=\"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp\"></div></div><ul tabindex=\"0\" class=\"menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow\"><li><a class=\"justify-between\">Profile <span class=\"badge\">New</span></a></li><li><a>Settings</a></li><li><a>Logout</a></li></ul></div></div></div><div id=\"content\" class=\"container mx-auto py-4\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html data-theme=\"light\"><head><title></title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link href=\"/static/index.css\" rel=\"stylesheet\" type=\"text/css\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = datastar.ScriptCDNLatest().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</head><body><div class=\"navbar bg-base-100\"><div class=\"navbar-start\"><div class=\"dropdown\"><div tabindex=\"0\" role=\"button\" class=\"btn btn-ghost lg:hidden\"><svg xmlns=\"http://www.w3.org/2000/svg\" class=\"h-5 w-5\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M4 6h16M4 12h8m-8 6h16\"></path></svg></div><ul tabindex=\"0\" class=\"menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow\"><li><a>Item 1</a></li><li><a>Parent</a><ul class=\"p-2\"><li><a>Submenu 1</a></li><li><a>Submenu 2</a></li></ul></li><li><a>Item 3</a></li></ul></div><a class=\"btn btn-ghost text-xl\">daisyUI</a></div><div class=\"navbar-center hidden lg:flex\"><ul class=\"menu menu-horizontal px-1\"><li><a>Item 1</a></li><li class=\"z-10\"><details><summary>Parent</summary><ul class=\"p-2 min-w-48\"><li><a>Submenu 1</a></li><li><a>Submenu 2</a></li></ul></details></li><li><a>Item 3</a></li></ul></div><div class=\"navbar-end\"><div class=\"dropdown dropdown-end\"><div tabindex=\"0\" role=\"button\" class=\"btn btn-ghost btn-circle avatar\"><div class=\"w-10 rounded-full\"><img alt=\"Tailwind CSS Navbar component\" src=\"https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp\"></div></div><ul tabindex=\"0\" class=\"menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow\"><li><a class=\"justify-between\">Profile <span class=\"badge\">New</span></a></li><li><a>Settings</a></li><li><a>Logout</a></li></ul></div></div></div><div id=\"content\" class=\"container mx-auto py-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
